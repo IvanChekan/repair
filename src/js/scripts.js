d6c91eed37b908eb585bbbfb89dbd521bc2997f1
Ivan <ivanchekan07@gmail.com>
2019-08-23 22:47:02 +0500

    Сверстал два блока, сделал слайдер

---
+++ src/js/scripts.js
@@ -0,0 +1,40 @@
+// Обработка и отправка через технологию 	Ajax
+$(document).ready(function () {
+	$('#offer-form').on('submit', function (event) {
+		event.preventDefault();
+		$.ajax({
+			url: 'mail.php',
+			type: 'POST',
+			data: $(this).serialize(),
+			success: function (data) {
+				$('.success').html(data + ',Ваша форма отправлена.')
+			}
+		});
+	});
+});
+
+
+// Слайдер портфолио 
+$(document).ready(function () {
+	$('.slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		prevArrow: $('.arrows__left'),
+		nextArrow: $('.arrows__right'),
+		responsive: [{
+				breakpoint: 1200,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+			},
+		]
+	});
+});
