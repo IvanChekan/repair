94aa473ebca8f64cb85f7396d6a15847b6fb253a
Ivan <ivanchekan07@gmail.com>
2019-08-25 17:19:22 +0500

    Доделал сайт, удаляю папки

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,25 @@
 // Обработка и отправка через технологию 	Ajax
 $(document).ready(function () {
+	$('#offer__form').validate({
+		rules: {
+			username: {
+				required: true,
+				minlength: 2
+			},
+			userphone: {
+				required: true,
+			}
+		},
+		messages: {
+			username: {
+				required: "укажите имя",
+				minlength: jQuery.validator.format("Осталось символов: 1")
+			},
+			userphone: {
+				required: "Укажите телефон",
+			}
+		},
+	});
 	$('#offer-form').on('submit', function (event) {
 		event.preventDefault();
 		$.ajax({
@@ -42,4 +62,38 @@ $(document).ready(function () {
 // Инициализация  WOW.JS
 $(document).ready(function () {
 	new WOW().init();
+});
+// Валидация форм
+$(document).ready(function () {
+	$('#brif-form').validate({
+		rules: {
+			username: {
+				required: true,
+				minlength: 2
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			userphone: {
+				required: true,
+			}
+		},
+		messages: {
+			username: {
+				required: "укажите имя",
+				minlength: jQuery.validator.format("Осталось символов: 1")
+			},
+			email: {
+				required: "Введите ваш email",
+				email: "Введите корректный email"
+			},
+			userphone: {
+				required: "Укажите телефон",
+			}
+		},
+	});
+
+	// Маска для телефона
+	$('.phone').mask('+7 (999) 999-99-99');
 });
